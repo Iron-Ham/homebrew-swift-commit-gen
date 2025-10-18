8e649591477fde7f10934421ce02a1b09472576d
Hesham Salman <hesham@hey.com>
2025-10-17 22:11:05 -0400

    Update swiftcommitgen to 0.1.1

--- swiftcommitgen.rb
+++ swiftcommitgen.rb
@@ -1,8 +1,8 @@
 class Swiftcommitgen < Formula
   desc "AI-powered Git commit message generator using Apple Intelligence"
   homepage "https://github.com/Iron-Ham/swift-commit-gen"
-  url "https://github.com/Iron-Ham/swift-commit-gen/archive/refs/tags/0.1.0.tar.gz"
-  sha256 "e03a5b438d17b3f7e74c2bcf2ad15a093e4003b804567d7606e40318d8b1a7b7"
+  url "https://github.com/Iron-Ham/swift-commit-gen/archive/refs/tags/0.1.1.tar.gz"
+  sha256 "c3cf9957dc2808fcb4120e5b488b78373edf9b4105b585c229a7fe1ebd3b331b"
   license "MIT"
   head "https://github.com/Iron-Ham/swift-commit-gen.git", branch: "main"
 
@@ -12,10 +12,6 @@ class Swiftcommitgen < Formula
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/swiftcommitgen"
-  end
-
-  test do
-    assert_match "USAGE: swiftcommitgen", shell_output("#{bin}/swiftcommitgen --help")
   end
 
   def caveats
@@ -37,5 +33,9 @@ class Swiftcommitgen < Formula
         --format     Output format (text or json)
         --help       Show all available options
     EOS
+  end
+
+  test do
+    assert_match "USAGE: swiftcommitgen", shell_output("#{bin}/swiftcommitgen --help")
   end
 end
